b9d459ccb0a1aa41eee6cfa97455cf00b5f27201
christophercodinga11y <christopherjack14@gmail.com>
2025-05-31 14:10:35 -0500

    adjusting the font size of the h3

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,8 +7,8 @@ const Home =()=> {
                     <div className="col-md-6">
                         <h2 className="name-text text-capitalize">about me</h2>
                         <p className="about-text">Hello! My name is Christopher Jackson, but most people call me Chris, I'm a Junior web developer at Mississippi Coding Academies. I enjoy working with computers and I am looking to expand my career options in this field.  From the academy, I have gained more experience in coding and also learned new coding languages. I am a 2020 high school graduate. While attending high school, I completed a 2-year course in Simulation and Animation Design. I enjoy playing videos games and watching TV during my free time.</p>
-                        <h3>Coding languages or frameworks that I have learned</h3>
-                        <ul className="mini-text list-unstyled">
+                        <h3 className="h3-heading">Coding languages or frameworks that I have learned</h3>
+                        <ul className="mini-text">
                             <li>HTML 5</li>
                             <li>CSS</li>
                             <li>BOOTSTRAP</li>

53b67bf4f5532d857452c2d4fe3134110acd694e
christophercodinga11y <christopherjack14@gmail.com>
2025-06-29 13:41:54 -0500

    enhance accessibility and improve project descriptions in Card component

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home =()=> {
                     <div className="col-md-6">
                         <h2 className="name-text text-capitalize">about me</h2>
                         <p className="about-text">Hello! My name is Christopher Jackson, but most people call me Chris, I'm a Junior web developer at Mississippi Coding Academies. I enjoy working with computers and I am looking to expand my career options in this field.  From the academy, I have gained more experience in coding and also learned new coding languages. I am a 2020 high school graduate. While attending high school, I completed a 2-year course in Simulation and Animation Design. I enjoy playing videos games and watching TV during my free time.</p>
-                        <h3 className="h3-heading">Coding languages or frameworks that I have learned</h3>
+                        <h3 className="h3-heading">Coding Languages & Frameworks that I've learned</h3>
                         <ul className="mini-text">
                             <li>HTML 5</li>
                             <li>CSS</li>
@@ -20,7 +20,7 @@ const Home =()=> {
                     </div>
                     <div className="col-md-6">
                         <figure className="headshot-figure">
-                            <img src="images/me.jpeg"alt="a picture of me" className="img-fluid image figure-image" />
+                            <img src="images/me.jpeg"alt="Christopher Jackson smiling" className="img-fluid image figure-image" />
                             
                         </figure>
                     </div>
--- src/data.js
+++ src/data.js
@@ -1,98 +1,98 @@
 export default [
-    {
-        id: 1, 
-        name: 'meme-generator',
-        imgUrl: 'meme-generator.png',
-        description: 'This is my memes generator page project with React, CSS and Bootstrap',
-        sourceCode: 'https://github.com/christophercoding-a11y/Meme-Generator',
-        livePage: 'https://meme-generator-phi-fawn.vercel.app/'
-    },
-    {
-        id: 2, 
-        name: 'thanksgiving',
-        imgUrl: 'thanksgiving.png',
-        description: 'This is my thanksgiving menu page project with, Html5, vanilla javascript, CSS and Bootstrap',
-        sourceCode: 'https://github.com/christophercoding-a11y/Thanksgiving-menu',
-        livePage: 'https://christophercoding-a11y.github.io/Thanksgiving-menu/'
-    },
-    {
-        id: 3, 
-        name: 'detroix',
-        imgUrl: 'detroix.png',
-        description: 'This my detroit page project with HTML5, CSS and Bootstrap',
-        sourceCode: 'https://github.com/christophercoding-a11y/detroixboostsrap',
-        livePage: 'https://christophercoding-a11y.github.io/detroixboostsrap/'
-    },
-    {
-        id: 4, 
-        name: 'movie-app',
-        imgUrl: 'movie-app.png',
-        description: 'This is my movie-app page project with, React, CSS and Bootstrap',
-        sourceCode: 'https://github.com/christophercoding-a11y/movie-app',
-        livePage: 'https://movie-app-sandy-three.vercel.app/'
-    },
-    {
-        id: 5, 
-        name: 'library app',
-        imgUrl: 'library_app.png',
-        description: 'This is my library page project with, Mysql, Express, React, CSS, and Bootstrap ',
-        sourceCode: 'https://github.com/christophercoding-a11y/final_project',
-        livePage: '#'
-    },
-    {
-        id: 6, 
-        name: 'restaurant',
-        imgUrl: 'restaurant.png',
-        description: 'This my restaurant page project with HTML5, vanilla javascript , CSS and Bootstrap',
-        sourceCode: 'https://github.com/christophercoding-a11y/restaurant',
-        livePage: 'https://christophercoding-a11y.github.io/restaurant/'
-    },
-    {
-        id: 7, 
-        name: 'bloom',
-        imgUrl: 'bloom.png',
-        description: 'This is my thanksgiving menu page project with, Html5, vanilla javascript, CSS and Bootstrap',
-        sourceCode: 'https://github.com/christophercoding-a11y/bloombs',
-        livePage: 'https://christophercoding-a11y.github.io/bloombs/'
-    },
-    {
-        id: 8, 
-        name: 'dice game',
-        imgUrl: 'dicegame.png',
-        description: 'This is my dice game project with, Html5, vanilla javascript, CSS and Bootstrap',
-        sourceCode: 'https://github.com/christophercoding-a11y/Dicegame',
-        livePage: 'https://christophercoding-a11y.github.io/Dicegame/'
-    },
-    {
-        id: 9, 
-        name: 'boxgame',
-        imgUrl: 'boxgame.png',
-        description: 'This is my boxgame project with, Html5, vanilla javascript, CSS and Bootstrap',
-        sourceCode: 'https://github.com/christophercoding-a11y/BoxGame',
-        livePage: 'https://christophercoding-a11y.github.io/BoxGame/'
-    },
-    {
-        id: 10, 
-        name: 'snake',
-        imgUrl: 'snakegame.png',
-        description: 'This is my snake game page project with, Html5, vanilla javascript, CSS and Bootstrap',
-        sourceCode: 'https://github.com/christophercoding-a11y/snakegame',
-        livePage: 'https://christophercoding-a11y.github.io/snakegame/'
-    },
-    {
-        id: 11, 
-        name: 'to-do-list',
-        imgUrl: 'todo.png',
-        description: 'This is my to-do-list project with, Html5, vanilla javascript, CSS and Bootstrap',
-        sourceCode: 'https://github.com/christophercoding-a11y/To-Do-List',
-        livePage: 'https://christophercoding-a11y.github.io/To-Do-List/'
-    },
-    {
-        id: 12, 
-        name: 'regex',
-        imgUrl: 'regex.png',
-        description: 'This is my password verification project with, Html5, vanilla javascript, CSS and Bootstrap',
-        sourceCode: 'https://github.com/christophercoding-a11y/Regex',
-        livePage: 'https://christophercoding-a11y.github.io/Regex/'
-    },
-]
+{
+    id: 1,
+    name: 'Meme Generator',
+    imgUrl: 'meme-generator.png',
+    description: 'A meme generator built with React, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/Meme-Generator',
+    livePage: 'https://meme-generator-phi-fawn.vercel.app/',
+},
+{
+    id: 2,
+    name: 'Thanksgiving Menu',
+    imgUrl: 'thanksgiving.png',
+    description: 'A Thanksgiving menu page built with HTML5, vanilla JavaScript, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/Thanksgiving-menu',
+    livePage: 'https://christophercoding-a11y.github.io/Thanksgiving-menu/',
+},
+{
+    id: 3,
+    name: 'Detroix',
+    imgUrl: 'detroix.png',
+    description: 'A Detroit-themed page built with HTML5, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/detroixboostsrap',
+    livePage: 'https://christophercoding-a11y.github.io/detroixboostsrap/',
+},
+{
+    id: 4,
+    name: 'Movie App',
+    imgUrl: 'movie-app.png',
+    description: 'A movie app built with React, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/movie-app',
+    livePage: 'https://movie-app-sandy-three.vercel.app/',
+},
+{
+    id: 5,
+    name: 'Library App',
+    imgUrl: 'library_app.png',
+    description: 'A full-stack library app built with MySQL, Express, React, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/final_project',
+    livePage: '#',
+},
+{
+    id: 6,
+    name: 'Restaurant Page',
+    imgUrl: 'restaurant.png',
+    description: 'A restaurant website built with HTML5, vanilla JavaScript, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/restaurant',
+    livePage: 'https://christophercoding-a11y.github.io/restaurant/',
+},
+{
+    id: 7,
+    name: 'Bloom',
+    imgUrl: 'bloom.png',
+    description: 'A themed menu page built with HTML5, vanilla JavaScript, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/bloombs',
+    livePage: 'https://christophercoding-a11y.github.io/bloombs/',
+},
+{
+    id: 8,
+    name: 'Dice Game',
+    imgUrl: 'dicegame.png',
+    description: 'A dice game built with HTML5, vanilla JavaScript, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/Dicegame',
+    livePage: 'https://christophercoding-a11y.github.io/Dicegame/',
+},
+{
+    id: 9,
+    name: 'Box Game',
+    imgUrl: 'boxgame.png',
+    description: 'A simple box game created with HTML5, vanilla JavaScript, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/BoxGame',
+    livePage: 'https://christophercoding-a11y.github.io/BoxGame/',
+},
+{
+    id: 10,
+    name: 'Snake Game',
+    imgUrl: 'snakegame.png',
+    description: 'A classic snake game made with HTML5, vanilla JavaScript, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/snakegame',
+    livePage: 'https://christophercoding-a11y.github.io/snakegame/',
+},
+{
+    id: 11,
+    name: 'To-Do List',
+    imgUrl: 'todo.png',
+    description: 'A simple to-do list application made with HTML5, vanilla JavaScript, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/To-Do-List',
+    livePage: 'https://christophercoding-a11y.github.io/To-Do-List/',
+},
+{
+    id: 12,
+    name: 'Regex Password Validator',
+    imgUrl: 'regex.png',
+    description: 'A password strength validator using regex, built with HTML5, vanilla JavaScript, CSS, and Bootstrap.',
+    sourceCode: 'https://github.com/christophercoding-a11y/Regex',
+    livePage: 'https://christophercoding-a11y.github.io/Regex/',
+},
+];

984360b95673ab6806b529c292a3021ed4f3bb8f
christophercodinga11y <christopherjack14@gmail.com>
2025-08-28 11:18:33 -0500

    add text decoration to about me text and technical skills text

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,8 +3,7 @@ const Card =(props)=> {
         <div className="col">
             <div className="card project-card h-100">
                 <img src={`/images/${props.imgUrl}`} alt={`${props.name} project screenshot`}className="img-fluid image" loading="lazy" />
-                
-                <div className="project-card card-body d-flex flex-column">
+                <div className="card-body d-flex flex-column">
                     <h2 className="card-title text-uppercase project-text">{props.name}</h2>
                     <p className="card-text">{props.description}</p>
                     <div className="button-group mt-auto">
--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ const Home =()=> {
                     </div>
                     <div className="col-md-6">
                         <figure className="headshot-figure">
-                            <img src="images/me.jpeg"alt="Christopher Jackson smiling" className="img-fluid image figure-image profile-pic" loading="lazy" />
+                            <img src="images/me.jpeg"alt="Christopher Jackson smiling" className="img-fluid image figure-image profile-pic"/>
                             <figcaption className="visually-hidden">Headshot of Christopher Jackson smiling</figcaption>
                         </figure>
                     </div>
